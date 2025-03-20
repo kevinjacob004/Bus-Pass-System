bccf60afb172080cba173451c2d20f496960606f
kevinjacob004 <kevinjacobmathew.004@gmail.com>
2025-03-20 22:54:51 +0530

    test

--- bus_pass-backend2/server.js
+++ bus_pass-backend2/server.js
@@ -1,4 +1,7 @@
-import 'dotenv/config';
+// import 'dotenv/config';
+import dotenv from 'dotenv';
+dotenv.config();
+
 import express from 'express';
 import cors from 'cors';
 import { errorHandler } from './middleware/error.js';
