6a794178381ce7b52a8917dccf69d9ebbee14152
kevinjacob004 <kevinjacobmathew.004@gmail.com>
2025-03-20 22:13:04 +0530

    test

--- bus_pass-backend2/config/db.js
+++ bus_pass-backend2/config/db.js
@@ -1,5 +1,5 @@
 // import { drizzle } from 'drizzle-orm/mysql2';
-import mysql from 'mysql2/promise';
+// import mysql from 'mysql2/promise';
 const { drizzle } = require("drizzle-orm/mysql2");
 // const mysql = require("mysql2/promise");
 
